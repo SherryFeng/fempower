b859ed735c69841a589646dc085b6784043ab1d5
Aditi <amp102@duke.edu>
2017-10-29 13:18:37 -0400

    javascript

--- js/Javascriptfinder.js
+++ js/Javascriptfinder.js
@@ -1,5 +1,6 @@
-import googlemaps
-from secret.py import key 
+
+//import googlemaps
+//from secret.py import key 
 
 var mentors = new Array ();
 var mentees = new Array ();
@@ -8,8 +9,8 @@ var current=[];
 var matches = new Array();
 var compare=[];
 
-var mentorcounter=0;
-var menteecounter=0;
+var mentorcounter=4;
+var menteecounter=6;
 
 var count1=0;
 var count2=0;
@@ -17,7 +18,7 @@ var count2=0;
 
 
 var nameInput = document.getElementById("name");
-var ageInput = document.getElementById("age");//
+var ageInput = document.getElementById("age");
 var addInput = document.getElementById("location");
 var mentorInput = document.getElementById("mentor");
 var fieldInput = document.getElementById("field");
@@ -26,7 +27,7 @@ var emailInput = document.getElementById("email");
 var interestInput = document.getElementById("interest"); 
 var messageBox = document.getElementById("display");
 
-var service = googlemaps.Client(key=key);
+/*var service = googlemaps.Client(key=key);*/
 
 //examples for testing
 mentors[0]=["Sheryl Sandberg","48", "Washington, DC", "mentor","tech", "startup"];
@@ -48,37 +49,30 @@ function insert() {
 		mentors[mentorcounter]= new Array (nameInput.value, ageInput.value, addInput.value, mentorInput.value, fieldInput.value, linkedInInput.value, emailInput.value, interestInput.value);
 		mentorcounter=mentorcounter+1;
 		for (j=0;j<mentees.length;j++){
-			if (count2<=3 && (mentees[j][4]==current[4] || mentees[j][7]==current[7]){
-				compare=mentees[j][2];
-				if(service.getDistanceMatrix[current[2],mentees[j][2]]{
-					matches[count2]= mentees[j];
-					count2=count2+1;
-				}
+			if (count2<=3 && ((mentees[j][4]==current[4]) || (mentees[j][7]==current[7]))){
+				matches[count2] = new Array(mentees[j][0],mentees[j][1],mentees[j][2],mentees[j][3],mentees[j][4],mentees[j][5],mentees[j][6],mentees[j][7]);
+				count2=count2+1;
 			}
 
-			else {
-				(alert("Sorry you have no matches! Please try again later"));
-			}
+			
 		}
 	}
 	else {
 		mentees[menteecounter] = new Array (nameInput.value, ageInput.value, addInput.value, mentorInput.value, fieldInput.value, linkedInInput.value, emailInput.value, interestInput.value);
 			menteecounter=menteecounter+1;
 			for (i=0;i<mentors.length;i++){
-			if (count1<=3 && (mentors[j][4]==current[4] || mentors[j][7]==current[7]){
-				compare=mentors[j][2];
-				if(service.getDistanceMatrix[current[2],mentors[i][2]]{
-					matches[count1]= mentees[j];
+			if (count1<=3 && ((mentors[i][4]==current[4]) || (mentors[i][7]==current[7]))){
+				/*compare=mentors[j][2];*/
+				/*if(service.getDistanceMatrix[current[2],mentors[i][2]]{
+					(alert("Success!"));*/
+					matches[count1]= new Array (mentors[i][0],mentors[i][1],mentors[i][2],mentors[i][3],mentors[i][4],mentors[i][5],mentors[i][6],mentors[i][7]);
 					count1=count1+1;
-				}
-			}
-			else {
-				(alert("Sorry you have no matches! Please try again later"));
+				//}
 			}
 		}
 
 	}
-}
+
 
 	names.push(nameInput.value);
 	clearAndShow();
@@ -90,10 +84,15 @@ function clearAndShow (){
 	nameInput.value="";
 	messageBox.innerHTML="";
 	messageBox.innerHTML+="Names: "+names.join(", ");
+
+	}
 	/*messageBox.innerHTML+="Mentors: "+mentInputs.join(",");*/
 	
 	
 }
-<script async defer
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfagwXweNt8Rd2sXlANtgbrQxgCEHQ5R4&callback=initMap"
+
+
+/*<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfagwXweNt8Rd2sXlANtgbrQxgCEHQ5R4&callback=initMap">
+</script>*/
 
